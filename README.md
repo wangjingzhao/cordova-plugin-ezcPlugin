13beabe66ddf88e5b2a0cf032e75bcd029a1245f
wangjingzhao <517346462@qq.com>
2021-04-02 14:42:29 +0800

    ios 推送获取和跳转

---
+++ README.md
@@ -0,0 +1,18 @@
+##cordova-plugin-ezcPlugin
+这个插件是做什么的？
+当你想问这个问题的时候，你就记住！你别用就对了！
+
+Demo
+
+
+###打开推送设置页demo
+cordova.plugins.ezcPlugin.openNoticeSetting(
+    function(data){
+        alert(data)
+})
+
+###检测是否允许了APP推送
+cordova.plugins.ezcPlugin.IfcanPush(
+    function(data){
+        alert(data)
+})
--- src/ios/cordova-plugin-ezcPlugin.m
+++ src/ios/cordova-plugin-ezcPlugin.m
@@ -2,14 +2,17 @@
 
 #import <Cordova/CDV.h>
 
-@interface cordova-plugin-ezcPlugin : CDVPlugin {
+@interface ezcPlugin : CDVPlugin {
   // Member variables go here.
 }
 
 - (void)coolMethod:(CDVInvokedUrlCommand*)command;
+- (void)IfcanPush:(CDVInvokedUrlCommand*)command;
+- (void)openNoticeSetting:(CDVInvokedUrlCommand*)command;
+
 @end
 
-@implementation cordova-plugin-ezcPlugin
+@implementation ezcPlugin
 
 - (void)coolMethod:(CDVInvokedUrlCommand*)command
 {
@@ -25,4 +28,46 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
+- (void)IfcanPush:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    //NSString* echo = [command.arguments objectAtIndex:0];
+    #define IOS8 ([[[UIDevice currentDevice] systemVersion] doubleValue] >=8.0 ? YES : NO)
+
+
+    if (IOS8) {
+        NSLog(@"%@", @"ios8");
+        if ([[UIApplication sharedApplication] currentUserNotificationSettings].types == UIRemoteNotificationTypeNone)
+        {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:false];
+        }
+        else
+        {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:true];
+        }
+    }
+    else
+    { // ios7 一下
+        NSLog(@"%@", @"ios7");
+        if ([[UIApplication sharedApplication] enabledRemoteNotificationTypes] == UIRemoteNotificationTypeNone)
+        {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:false];
+        }
+        else
+        {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:true];
+        }
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
+- (void)openNoticeSetting:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:true];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
 @end
